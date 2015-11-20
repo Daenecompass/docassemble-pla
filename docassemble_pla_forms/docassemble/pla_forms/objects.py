8311af55c0e31c0728b118593aaec7eae0e94990
Jonathan Pyle <jhpyle@gmail.com>
2015-11-20 09:08:09 -0500

    updated surveys to new version of docassemble

--- docassemble_pla_forms/docassemble/pla_forms/objects.py
+++ docassemble_pla_forms/docassemble/pla_forms/objects.py
@@ -46,7 +46,7 @@ class LegalServer(DAObject):
         return
     def lookup_unique_id(self, unique_id):
         cur = self.conn.cursor()
-        cur.execute("""select m.identification_number, up.first as sfname, up.last as slname, up.email as semail, c.first as cfname, c.last as clname, c.email as cemail, map.office_name, lookup_citizenship_statuses.name as cstatus, up.phone_business as sphone from matter as m left outer join matter_assignment_primary as map on (m.id=map.matter_id) left outer join users as u on (map.user_id=u.id) left outer join person as up on (u.person_id=up.id) left outer join person as c on (m.person_id=c.id) left outer join lookup_citizenship_statuses on (m.citizenship_status=lookup_citizenship_statuses.id) where m.unique_id=%s""", (str(unique_id),))
+        cur.execute("""select m.identification_number, up.first as sfname, up.last as slname, up.email as semail, c.first as cfname, c.last as clname, c.email as cemail, map.office_name, lookup_citizenship_statuses.name as cstatus, up.phone_business as sphone, lookup_legal_problem_code.name as pcode from matter as m left outer join matter_assignment_primary as map on (m.id=map.matter_id) left outer join users as u on (map.user_id=u.id) left outer join person as up on (u.person_id=up.id) left outer join person as c on (m.person_id=c.id) left outer join lookup_citizenship_statuses on (m.citizenship_status=lookup_citizenship_statuses.id) left outer join lookup_legal_problem_code on (m.legal_problem_code = lookup_legal_problem_code.id) where m.unique_id=%s""", (str(unique_id),))
         rows = cur.fetchall()
         result = dict()
         for row in rows:
@@ -60,6 +60,7 @@ class LegalServer(DAObject):
             result['office_name']           = row[7]
             result['cstatus']               = row[8]
             result['sphone']                = row[9]
+            result['pcode']                 = row[10]
         if 'identification_number' not in result:
             raise Exception("Could not retrieve information about unique_id")
         return(result)
